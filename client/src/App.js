f2c67f99149c6690504095e49f467bf6166d8939
Rurucode <rubenlage12@gmail.com>
2022-01-15 11:23:58 +0100

    Login completado

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Main/Login'
+import Logout from './components/Main/Logout/Logout';
+import Profile from './components/Main/Profile/Profile';
+import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
+  const {isAuthenticated} = useAuth0();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? <><Profile/><Logout/></> : <Login/>}
     </div>
   );
 }
