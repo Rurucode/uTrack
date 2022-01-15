08896ba82df3d984d72f02c745be71ae824af040
Rurucode <rubenlage12@gmail.com>
2022-01-15 12:13:34 +0100

    Login completado

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      {isAuthenticated ? <><Profile/><Logout/></> : <Login/>}
+      {isAuthenticated ? <><Profile/><Logout/> </> : <Login/>}
     </div>
   );
 }
